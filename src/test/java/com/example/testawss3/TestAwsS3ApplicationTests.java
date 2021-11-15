b57df9eaead68558ebb0c767dc0e9427f93a8357
makewheels <makewheels>
2021-11-16 00:19:04 +0800

    init

---
+++ src/test/java/com/example/testawss3/TestAwsS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testawss3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAwsS3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
